d3d4d52d440e45629d83094caf1d4abde53a3657
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2024-07-22 09:08:01 -0700

    Remove update.sh

--- update.sh
+++
@@ -1,8 +0,0 @@
-git fetch --all
-git reset --hard origin/main
-
-. .venv/bin/activate
-pip install -r requirements.txt
-pip install -r requirements-prod.txt
-
-sudo systemctl restart ferry-planner.service
